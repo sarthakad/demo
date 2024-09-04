4e7de02d069a9ddf15aebe63d2325136f8a9d83d
Sarthak <Sarthakad777@gmail.com>
2024-09-04 06:49:36 +0545

    new file saved

---
+++ face.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox as mess
+import tkinter.simpledialog as tsd
+
+
+window = tk.Tk()
+window.geometry("1280x720")
+window.resizable(True,False)
+window.title("Attendance System")
+window.configure(background='#2d420a')
